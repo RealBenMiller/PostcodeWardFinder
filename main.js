e3a774bbaa213a17c2335b3ee96ffd2fd8da3f12
RealBenMiller <31767890+RealBenMiller@users.noreply.github.com>
2019-07-03 13:21:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const postCodeURL = 'https://api.postcodes.io/postcodes/';
+let postCode = '';
+let tableContents = '';
+
+const applyPostcodes = () => {  
+    postCode = document.getElementById('input').value;
+    
+    var postCodesArray = postCode.split('\n');
+
+    if (postCode) {
+        $(".clear").css("display", "block");
+        
+    $("#find").css("display", "none");
+        for (code in postCodesArray) {
+            let searchURL = postCodeURL + postCodesArray[code];
+            $.ajax({async: false, url: searchURL, success: function (returns){
+                let ward = returns.result.admin_ward;
+    
+                tableContents += "<tr><td>" + postCodesArray[code] + "</td><td>" + ward + "</td></tr>";
+            }
+        });
+    };
+
+    
+};
+
+$("#tableBody").html(tableContents);
+
+    if(postCode){
+        $("table").css("display", "block");
+    }
+    
+}
+
+const makeClear = () => {
+
+    $(".clear").css("display", "none");
+    $("#find").css("display", "block");
+    tableContents = '';
+    $(".results").html(tableContents);
+    document.getElementById('input').value = '';
+    $("table").css("display", "none");
+
+}
